6d3276e020c1968d5b07515e7cfb9d26cea2966b
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2023-07-05 11:49:23 -0300

    feat: next auth config providers

---
+++ src/app/providers/auth.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { SessionProvider } from "next-auth/react"
+import { ReactNode } from "react"
+type Props = {
+  children: ReactNode
+}
+export const NextAuthProvider = ({ children }: Props) => {
+  return <SessionProvider>{children}</SessionProvider>
+}
