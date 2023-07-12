5f66343650fe96a16941595d353461005728cf54
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2023-07-11 23:24:24 -0300

    feat: slide trip imagens

--- src/app/trips/[tripId]/components/TripHeader.tsx
+++ src/app/trips/[tripId]/components/TripHeader.tsx
@@ -1,19 +1,48 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import ReactCountryFlag from 'react-country-flag'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 import { Trip } from '@prisma/client'
 import Link from 'next/link'
-
+import { Navigation, Pagination } from 'swiper/modules'
 export const TripHeader = ({ trip }: { trip: Trip }) => {
   return (
     <div className="w-full">
-      <Image
-        className="mb-5 h-[300px] w-full object-cover"
-        src={trip.coverImage}
-        alt={trip.name}
-        width={393}
-        height={208}
-      />
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation={true}
+        pagination={true}
+        modules={[Navigation, Pagination]}
+      >
+        <SwiperSlide>
+          <Image
+            className="mb-5 h-[300px] w-full object-cover"
+            src={trip.coverImage}
+            alt={trip.name}
+            width={393}
+            height={208}
+          />
+        </SwiperSlide>
+        {trip.imagesUrl.map((image, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <Image
+                className="mb-5 h-[300px] w-full object-cover"
+                src={image}
+                alt={trip.name}
+                width={393}
+                height={208}
+              />
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
       <div className="flex flex-col gap-2 px-5">
         <div className="flex items-center gap-3">
           <Link className="w-max transition-all hover:scale-110" href={'/'}>
