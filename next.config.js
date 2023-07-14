25b0e82395421acf9f279c79555296cc2bd0e910
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2023-07-14 03:09:15 -0300

    feat: migration and configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'images.unsplash.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'images.unsplash.com',
+      'imgcy.trivago.com',
+      'cf.bstatic.com',
+      'a0.muscache.com',
+      'www.trivago.com.br',
+    ],
   },
 }
 
